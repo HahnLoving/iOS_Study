065c46ad8ca71f47fcc01a1e7d8cb62feb0eddb1
Hahn <z253154650@qq.com>
2022-06-13 09:30:14 +0800

    SwiftUI
    SwiftUI

---
+++ 体系_SwiftUI/体系_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  体系_SwiftUI
+//
+//  Created by z Hahn on 2022/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
