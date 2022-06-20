ea04ae7a7e1545a3423eca4f9e7a8f261597c0bf
Hahn <z253154650@qq.com>
2022-06-20 15:20:19 +0800

    swift generic
    swift generic

--- 体系_Swift/体系_Swift/ViewController.swift
+++ 体系_Swift/体系_Swift/ViewController.swift
@@ -11,7 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: 泛型例子
+        showGenericNumList(numList: [1,2,3])
+        showGenericStringList(strList: ["你","我","他"])
+        
+        showGenericTList(tList: [1,2,3])
+        showGenericTList(tList: ["你","我","他"])
+        
+        // MARK: 关联类使用范性
+        let sInt = StatesInt()
+        sInt.demo(item: 1)
+        
+        let sT = StatesT<String>()
+        sT.demo(item: "good")
+        
+        // MARK: Where 语句
+        let sT1 = StatesT<String>()
+        let sT2 = StatesT<Int>()
+        // 类型不相同错误 Global function 'ZStateTypeWhere(z1:z2:)' requires the types 'StatesInt.StateType' (aka 'Int') and 'String' be equivalent
+//        ZStateTypeWhere(z1: sT1, z2: sT2)
+        ZStateTypeWhere(z1: sT1, z2: sT1)
+        
+        // MARK: 泛型和Ang
+        showGeneric(n: 1)
+        showAny(n: 1)
     }
 
 
